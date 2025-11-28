61cb49ef5fd552a2f5e24010dfd1d842820db289
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-11-28 03:09:03 -0800

    -

--- .sh
+++ .sh
@@ -9,13 +9,35 @@ flag() {
 GRAALVM_HOME="/opt/graalvm-community-openjdk-25.0.1+8.1"
 alias jlink="$GRAALVM_HOME/bin/jlink"
 alias jpackage="$GRAALVM_HOME/bin/jpackage"
-rm -rf bin dist
-mkdir -p bin dist
+mkdir -p bin
+MF=$(mktemp)
+JAR=$(mktemp)
+JARvar=${JAR//[.\/]/_}
+H=$(mktemp)
+C=src/launcher.c
 javac $(find src -name "*.java") -d bin
-echo "Main-Class: app.App" > manifest.mf
-jar cfm dist/App.jar manifest.mf -C bin .
-rm manifest.mf
-xxd -i dist/App.jar > src/jar.h
-gcc src/launcher.c -o dist/launcher
-cp dist/launcher app
+echo "Main-Class: app.App" > $MF
+jar cfm $JAR $MF -C bin .
+xxd -i $JAR > $H
+cat << EOF > $C
+#include <stdio.h>
+#include <stdlib.h>
+#include "$H"
+int main()
+{
+	FILE *f = fopen("$JAR", "wb");
+	if (!f)
+	{
+		perror("fopen");
+		return 1;
+	}
+	fwrite(${JARvar}, 1, ${JARvar}_len, f);
+	fclose(f);
+	int ret = system("\$GRAALVM_HOME/bin/java -jar $JAR");
+	remove("$JAR");
+	return ret;
+}
+EOF
+gcc "$C" -o app
+rm -r bin $MF $JAR $H $C
 ./app
